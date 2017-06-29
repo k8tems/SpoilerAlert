33614ddc2686a636ef96c34fb7653036bbb9fd96
hiro <aiueo@yahoo.co.jp>
2017-06-29 14:25:46 +0900

    Rename method

--- filter.py
+++ filter.py
@@ -1,22 +1,6 @@
 from PIL import Image, ImageFilter, ImageFont, ImageDraw
 
 
-def get_text_pos(img_size, text_size):
-    img_width, img_height = img_size
-    img_center_x = img_width / 2
-    img_center_y = img_height / 2
-    text_width, text_height = text_size
-    return img_center_x - text_width / 2, img_center_y - text_height / 2
-
-
-def find_fitting_font(font_file, recommended_width, caption):
-    for i in range(1, 500):
-        font = ImageFont.truetype(font_file, size=i)
-        if font.getsize(caption)[0] > recommended_width:
-            return font
-    assert()
-
-
 def resize_img(img, aspect_ratio):
     img_width, img_height = img.size
     return img.resize((int(img_width * aspect_ratio), int(img_height * aspect_ratio)))
@@ -43,7 +27,23 @@ def blur_img(orig_img):
     return orig_img.filter(ImageFilter.GaussianBlur(radius=40))
 
 
-def filter_img(img, caption, font_file):
+def get_text_pos(img_size, text_size):
+    img_width, img_height = img_size
+    img_center_x = img_width / 2
+    img_center_y = img_height / 2
+    text_width, text_height = text_size
+    return img_center_x - text_width / 2, img_center_y - text_height / 2
+
+
+def find_fitting_font(font_file, recommended_width, caption):
+    for i in range(1, 500):
+        font = ImageFont.truetype(font_file, size=i)
+        if font.getsize(caption)[0] > recommended_width:
+            return font
+    assert()
+
+
+def draw_caption(img, caption, font_file):
     font = find_fitting_font(font_file,img.size[0] / 2, caption)
     draw = ImageDraw.Draw(img)
     draw.text(get_text_pos(img.size, font.getsize(caption)), caption, font=font)
@@ -55,12 +55,12 @@ def run(caption, in_file, out_file, font_file, aspect_ratio=1.0):
     orig_img = resize_img(orig_img, aspect_ratio)
 
     filtered_img = blur_img(orig_img)
-    blurred_img = filter_img(filtered_img, caption, font_file)
+    filtered_img = draw_caption(filtered_img, caption, font_file)
 
     blur_duration = 2000
     blurred_frames = 10
 
     gif = Gif()
-    gif += [(blurred_img, blur_duration / blurred_frames)] * blurred_frames
+    gif += [(filtered_img, blur_duration / blurred_frames)] * blurred_frames
     gif.append((orig_img, 30000))
     gif.save(out_file)
