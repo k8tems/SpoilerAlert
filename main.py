c8baf27a0bb224c7f1771762d625736d354ba8c9
k8tems <k8tems@live.com>
2017-06-29 11:54:25 +0900

    Specify duration

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from PIL import Image, ImageFilter
 if __name__ == '__main__':
     in_file = sys.argv[1]
     out_file = sys.argv[2]
-    img = Image.open (in_file)
+    img = Image.open(in_file)
     blurred_img = img.filter(ImageFilter.GaussianBlur(radius=40))
-    img.save(out_file, save_all=True, append_images=[blurred_img])
+    img.save(out_file, save_all=True, duration=[5000,30000], append_images=[blurred_img])
