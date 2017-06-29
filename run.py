f139dce832713e03d182044b81107cca54682249
k8tems <k8tems@live.com>
2017-06-29 13:43:41 +0900

    Serialize file size

--- run.py
+++ run.py
@@ -2,7 +2,18 @@ import os
 import filter
 
 
+def serialize_file_size(file_size):
+    sizes = [
+        (1000000000, 'GB'),
+        (1000000, 'MB'),
+        (10000, 'KB'),
+    ]
+    for s, t in sizes:
+        if file_size > s:
+            return '%f %s' % (file_size / s, t)
+
+
 if __name__ == '__main__':
     out_file = 'out.gif'
     filter.run('FFXVネタバレ', 'test.png', out_file, 'font.ttf')
-    print('ファイルサイズ:', os.path.getsize(out_file))
+    print('ファイルサイズ:', serialize_file_size(os.path.getsize(out_file)))
