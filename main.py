10dc9d5f72fcd9159318cfe6d0c71f38b0e8e2a1
hiro <aiueo@yahoo.co.jp>
2017-06-29 11:42:14 +0900

    Blur image

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
 
 if __name__ == '__main__':
     img = Image.open ('test.png')
+    blurred_img = img.filter(ImageFilter.GaussianBlur(radius=40))
+    blurred_img.show()
     img.show()
