5f4c6540fd47c4e899203b2115bc9ee8ba396ff0
hiro <aiueo@yahoo.co.jp>
2017-06-29 11:45:47 +0900

    Create gif

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from PIL import Image, ImageFilter
 if __name__ == '__main__':
     img = Image.open ('test.png')
     blurred_img = img.filter(ImageFilter.GaussianBlur(radius=40))
-    blurred_img.show()
-    img.show()
+    img.save('out.gif', save_all=True, append_images=[blurred_img])
