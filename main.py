1312d87031e23194c5b2138c11219898aa98096e
hiro <aiueo@yahoo.co.jp>
2017-06-29 11:59:51 +0900

    Draw caption on image

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
 
 
 if __name__ == '__main__':
     in_file = sys.argv[1]
     out_file = sys.argv[2]
+    caption = 'FFXV'
     img = Image.open(in_file)
     blurred_img = img.filter(ImageFilter.GaussianBlur(radius=40))
+    font = ImageFont.truetype("arial")
+    draw = ImageDraw.Draw(blurred_img)
+    draw.text((10, 10), caption, font=font)
     img.save(out_file, save_all=True, duration=[5000,30000], append_images=[blurred_img])
