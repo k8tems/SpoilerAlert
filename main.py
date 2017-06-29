94ab925f93c722cad9539533906f2296d0fc80f4
k8tems <k8tems@live.com>
2017-06-29 12:07:01 +0900

    Take text size into account

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import sys
 from PIL import Image, ImageFilter, ImageFont, ImageDraw
 
 
-def get_text_pos(img_size, font):
+def get_text_pos(img_size, text_size):
     img_width, img_height = img_size
     img_center_x = img_width / 2
     img_center_y = img_height / 2
-    return (img_center_x, img_center_y)
+    text_width, text_height = text_size
+    return (img_center_x - text_width, img_center_y - text_height)
 
 
 if __name__ == '__main__':
@@ -17,6 +18,6 @@ if __name__ == '__main__':
     blurred_img = img.filter(ImageFilter.GaussianBlur(radius=40))
     font = ImageFont.truetype("arial", size=100)
     draw = ImageDraw.Draw(blurred_img)
-    draw.text(get_text_pos(img.size, font), caption, font=font)
+    draw.text(get_text_pos(img.size, font.getsize(caption)), caption, font=font)
     blurred_img.save(out_file, save_all=True, duration=[3000, 30000], append_images=[img])
     blurred_img.show()
