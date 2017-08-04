cc14cc8aa6d329b016c39b265ed758f41762070c
k8tems <k8tems@live.com>
2017-08-04 20:18:02 +0900

    Use separate mp4 files

--- filter.py
+++ filter.py
@@ -146,13 +146,14 @@ def main():
     if video.is_video(args.in_file):
         frame_path = get_temp_path('temp.png')
         gif_path = get_temp_path('temp.gif')
-        mp4_path = get_temp_path('temp.mp4')
+        mp4_path_1 = get_temp_path('temp1.mp4')
+        mp4_path_2 = get_temp_path('temp2.mp4')
         video.get_first_frame(args.in_file, frame_path)
         orig_img = Image.open(frame_path)
         gif = filter_image(orig_img, args.caption, args.resize_ratio, args.settings_file, args.font_file)
         gif.save(gif_path)
-        video.gif_to_mp4(gif_path, mp4_path)
-        video.merge_videos(mp4_path, args.in_file, args.out_file)
+        video.gif_to_mp4(gif_path, mp4_path_1, mp4_path_2)
+        video.merge_videos(mp4_path_2, args.in_file, args.out_file)
     else:
         orig_img = Image.open(args.in_file)
         gif = filter_image(orig_img, args.caption, args.resize_ratio, args.settings_file, args.font_file)
--- video.py
+++ video.py
@@ -13,9 +13,10 @@ def is_video(file):
     return file.endswith('mp4')
 
 
-def gif_to_mp4(src, dest):
-    run_ffmpeg('-i "%s" "%s"' % (src, dest))
-    run_ffmpeg('-i "%s" -f lavfi -i aevalsrc=0 -shortest -y "%s"' % (dest, dest))
+def gif_to_mp4(src, dest1, dest2):
+    run_ffmpeg('-f gif -i "%s" "%s"' % (src, dest1))
+    # Decoding will fail for this command if input and output file is the same
+    run_ffmpeg('-i "%s" -f lavfi -i aevalsrc=0 -shortest -y "%s"' % (dest1, dest2))
 
 
 def merge_videos(src1, src2, dest):
@@ -23,4 +24,4 @@ def merge_videos(src1, src2, dest):
 
 
 if __name__ == '__main__':
-    gif_to_mp4('mp4_out.gif', 'out.mp4')
+    gif_to_mp4('out.gif', 'out.mp4', 'out2.mp4')
