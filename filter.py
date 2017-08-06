1221c11a270ee780cac2de06b56913e41a84a0f8
k8tems <k8tems@users.noreply.github.com>
2017-08-06 12:47:51 +0900

    Improve video quality (#6)

--- filter.py
+++ filter.py
@@ -148,20 +148,20 @@ def parse_args():
 def process_video(args):
     # `NamedTemporaryFile`はWindowsだとサブプロセスから開けないので自分で実装する必要がある
     # https://stackoverflow.com/questions/15169101/how-to-create-a-temporary-file-that-can-be-read-by-a-subprocess
+    # サブルーチン内で一時ファイルを生成/削除するより
+    # ここで全部生成して明確にレイヤー分けした方が小回りが利く気がする
     with TemporaryDirectory() as temp_dir, \
             TemporaryFile(temp_dir, 'png') as frame_path, \
             TemporaryFile(temp_dir, 'gif') as filtered_path, \
-            TemporaryFile(temp_dir, 'mp4') as inaudible_video_path, \
-            TemporaryFile(temp_dir, 'mp4') as audible_video_path:
+            TemporaryFile(temp_dir, 'mp4') as filtered_video_path:
         logger.info('temp_dir ' + temp_dir)
         logger.info('filtered_path ' + filtered_path)
-        logger.info('inaudible_video_path ' + inaudible_video_path)
         video.save_first_frame(args.in_file, frame_path)
         orig_img = Image.open(frame_path)
         gif = filter_image(orig_img, args.caption, args.settings_file, args.font_file)
         gif.save(filtered_path)
-        video.convert_from_gif(filtered_path, inaudible_video_path, audible_video_path)
-        video.merge(audible_video_path, args.in_file, args.out_file)
+        video.convert_from_gif(filtered_path, filtered_video_path)
+        video.merge(filtered_video_path, args.in_file, args.out_file)
 
 
 def process_image(args):
